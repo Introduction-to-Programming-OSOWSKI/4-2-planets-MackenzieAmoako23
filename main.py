a2cd575366423b5068e6f30bcd4b29ec01609202
MackenzieAmoako23 <831590@mystma.org>
2021-02-17 16:05:27 +0000

    4-2 planets -1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def planets(x):
+    planet = [ "mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+    for i in range(0, len(x)):
+        if x == planet[i -1]:
+            return i
+
+    return  x +" " "is not a planet"
+print(planets("russia"))
+
